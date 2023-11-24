915172646af5724669224bd34d6f89e5c8e98f56
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-11-23 23:20:11 -0800

    verbose err.c

--- src/err.c
+++ src/err.c
@@ -1,14 +1,23 @@
 #include "err.h"
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+static void print(char *type, char *s1, char *s2)
+{
+    fprintf(stderr, "%s: %s\n", type, __stroserror(errno));
+    fprintf(stderr, s1, s2);
+    fprintf(stderr, "\n");
+}
 
 void err(int eno, char *s1, char *s2)
 {
-    printf(s1, s2);
+    print("Error", s1, s2);
     exit(eno);
 }
 
 void warn(char *s1, char *s2)
 {
-    printf(s1, s2);
+    print("Warning", s1, s2);
 }
